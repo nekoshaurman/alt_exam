a8378acc4cd104d796402db56a45cf41950aa44e
umlerr <ugrumov.misha@yandex.ru>
2022-12-18 15:17:44 +0300

    Deleted useless things

--- ECEF.py
+++ ECEF.py
@@ -1,74 +1,22 @@
-import pyproj as proj
-import numpy as np
-import scipy.spatial.transform
+import navpy as nv
 
 
 def from_wgs84_to_ecef(lat, lon, alt):
-    transformer = proj.Transformer.from_crs(
-        {"proj": 'latlong', "ellps": 'WGS84', "datum": 'WGS84'},
-        {"proj": 'geocent', "ellps": 'WGS84', "datum": 'WGS84'},
-    )
-    x, y, z = transformer.transform(lat, lon, alt, radians=False)
-    return x, y, z
+    ecef = nv.lla2ecef(lat, lon, alt)
+    return ecef[0], ecef[1], ecef[2]
 
 
 def from_ecef_to_wgs84(x, y, z):
-    transformer = proj.Transformer.from_crs(
-        {"proj": 'geocent', "ellps": 'WGS84', "datum": 'WGS84'},
-        {"proj": 'latlong', "ellps": 'WGS84', "datum": 'WGS84'},
-    )
-    lat, lon, alt = transformer.transform(x, y, z, radians=False)
-    return lat, lon, alt
+    ecef = [x, y, z]
+    lla = nv.ecef2lla(ecef)
+    return lla[0], lla[1], lla[2]
 
 
-def from_wgs84_to_enu(lat_pixel, lon_pixel, alt_pixel, lat_drone, lon_drone, alt_drone):
-    transformer = proj.Transformer.from_crs(
-        {"proj": 'latlong', "ellps": 'WGS84', "datum": 'WGS84'},
-        {"proj": 'geocent', "ellps": 'WGS84', "datum": 'WGS84'},
-    )
-    x_drone, y_drone, z_drone = transformer.transform(lon_drone, lat_drone, alt_drone, radians=False)
-    x_pixel, y_pixel, z_pixel = transformer.transform(lon_pixel, lat_pixel, alt_pixel, radians=False)
-    vec = np.array([[x_pixel - x_drone, y_pixel - y_drone, z_pixel - z_drone]]).T
+def from_ecef_to_ned(x, y, z, lat, lon, alt):
+    ecef = [x, y, z]
+    ned = nv.ecef2ned(ecef, lat, lon, alt)
+    return ned[0], ned[1], ned[2]
+# 34.00000048333403, -117.33356934722197, 251.70201122574508
+# 34.0000534361111, -117.333547744444, 234.052
 
-    rot1 = scipy.spatial.transform.Rotation.from_euler('x', -(90 - lat_pixel),
-                                                       degrees=True).as_matrix()  # angle*-1 : left handed *-1
-    rot3 = scipy.spatial.transform.Rotation.from_euler('z', -(90 + lon_pixel),
-                                                       degrees=True).as_matrix()  # angle*-1 : left handed *-1
-
-    rotMatrix = rot1.dot(rot3)
-
-    enu = rotMatrix.dot(vec).T.ravel()
-    return enu.T
-
-
-def from_enu_to_wgs84(e, n, u, lat_drone, lon_drone, alt_drone):
-    transformer1 = proj.Transformer.from_crs(
-        {"proj": 'latlong', "ellps": 'WGS84', "datum": 'WGS84'},
-        {"proj": 'geocent', "ellps": 'WGS84', "datum": 'WGS84'},
-    )
-    transformer2 = proj.Transformer.from_crs(
-        {"proj": 'geocent', "ellps": 'WGS84', "datum": 'WGS84'},
-        {"proj": 'latlong', "ellps": 'WGS84', "datum": 'WGS84'},
-    )
-
-    x_drone, y_drone, z_drone = transformer1.transform(lon_drone, lat_drone, alt_drone, radians=False)
-    ecef_org = np.array([[x_drone, y_drone, z_drone]]).T
-
-    rot1 = scipy.spatial.transform.Rotation.from_euler('x', -(90 - lat_drone),
-                                                       degrees=True).as_matrix()  # angle*-1 : left handed *-1
-    rot3 = scipy.spatial.transform.Rotation.from_euler('z', -(90 + lon_drone),
-                                                       degrees=True).as_matrix()  # angle*-1 : left handed *-1
-
-    rotMatrix = rot1.dot(rot3)
-
-    ecefDelta = rotMatrix.T.dot(np.array([[e, n, u]]).T)
-    ecef = ecefDelta + ecef_org
-    lon, lat, alt = transformer2.transform(ecef[0, 0], ecef[1, 0], ecef[2, 0], radians=False)
-
-    return lat, lon, alt
-
-
-#e, n, u = from_wgs84_to_enu(59.96, 30.20, 0, 59.973017, 30.220557, 50)
-#print(e, n, u)
-#lat, lon, alt = from_enu_to_wgs84(e, n, u, 59.973017, 30.220557, 50)
-#print(lat, lon, alt)
+# print(nv.lla2ned(34.0000534361111, -117.333547744444, 234.052, 34.00000048333403, -117.33356934722197, 251.70201122574508))
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Camera:
         self.roll = roll  # roll(degrees)
         self.pitch = pitch  # pitch(degrees)
         self.yaw = yaw  # yaw(°degrees)
-        self.focus = f / 1000  # focal length(mm) ---> convert to metres
+        self.focus = f  # focal length(mm) ---> convert to metres
         self.matrix_x = m_a  # matrix side, px
         self.matrix_y = m_b  # matrix side, px
         self.pixel_x = px  # px - matrix_x/2  # pixel X coordinate in the image, px
@@ -25,59 +25,29 @@ class Camera:
             [0, self.focus, self.matrix_y / 2],
             [0, 0, 1]
         ])
-        # print("\n", intrinsic_matrix)
         intrinsic_matrix_inverse = np.linalg.inv(intrinsic_matrix)
-        # print("\n", intrinsic_matrix_inverse)
         pixel_coord = np.array([
             [self.pixel_x],
             [self.pixel_y],
             [1]
         ])
-        # print("\n", pixel_coord)
         camera_coordinates = intrinsic_matrix_inverse.dot(pixel_coord)
-        # print("\n", from_pixel_to_camera)
-        camera_coordinates = camera_coordinates * self.alt
-        # print("\n", from_pixel_to_camera)
-        camera_coordinates[0][0] /= 1000
-        camera_coordinates[1][0] /= 1000
+        #camera_coordinates = camera_coordinates  * self.alt
+        #camera_coordinates[0][0] /= 1000
+        #camera_coordinates[1][0] /= 1000
         return camera_coordinates
-
-    def get_rotating_axes(self): #NOT READY YET
-        a = -self.lon
-        b = -2 * self.lat
-        c = self.lon + 180
-        # clockwise camera yaw
-        r_z = np.array([
-            [cos(a), -sin(a), 0],
-            [sin(a), cos(a), 0],
-            [0, 0, 1]
-        ])
-        # counterclockwise camera pitch
-        r_y = np.array([
-            [cos(b), 0, sin(b)],
-            [0, 1, 0],
-            [-sin(b), 0, cos(b)]
-        ])
-        # counterclockwise camera yaw 2
-        r_z2 = np.array([
-            [cos(c), -sin(c), 0],
-            [sin(c), cos(c), 0],
-            [0, 0, 1]
-        ])
-        rotate_matrix = (r_z.dot(r_y)).dot(r_z2)
-        return rotate_matrix #
 
     # Точка в мировых координатах
     def get_world_coordinates(self):
         yaw = 0
         pitch = 0
         roll = 0
-        camera_x_ecef, camera_y_ecef, camera_z_ecef = ECEF.from_wgs84_to_ecef(self.lat, self.lon, self.alt)
+        drone_x_ecef, drone_y_ecef, drone_z_ecef = ECEF.from_wgs84_to_ecef(self.lat, self.lon, self.alt)
         # coordinates of camera in world coordinates
         t = np.array([
-            [camera_x_ecef],
-            [camera_y_ecef],
-            [camera_z_ecef]
+            [drone_x_ecef],
+            [drone_y_ecef],
+            [drone_z_ecef]
         ])
         # clockwise camera yaw
         r_z = np.array([
@@ -103,17 +73,15 @@ class Camera:
         camera_coordinates = camera.get_camera_coordinates()
         camera_coordinates_ecef = rotate_matrix_inverse.dot(camera_coordinates)
 
-        rotate_matrix_2 = camera.get_rotating_axes()
+        world_coordinates_ecef = camera_coordinates_ecef + t
 
-        world_coordinates = camera_coordinates_ecef + t
-        print("\n", t)
-        return world_coordinates
+        return world_coordinates_ecef
 
 
 if __name__ == "__main__":
-    f = 1000
+    f = 60  # in mm
     matrix_x, matrix_y = 1920, 1080  # in px
-    px = 1000
+    px = 200
     py = 1000
     lat = 59.973017 # degrees
     lon = 30.220557  # degrees
@@ -122,6 +90,7 @@ if __name__ == "__main__":
     pitch = 0
     yaw = 0
     camera = Camera(lat, lon, alt, roll, pitch, yaw, px, py, f, matrix_x, matrix_y)
-    print("\n Coordinates relative to the camera\n", camera.get_camera_coordinates())
-    print("\n", camera.get_world_coordinates())
-    print("\n", ECEF.from_ecef_to_wgs84(*camera.get_world_coordinates()[0], *camera.get_world_coordinates()[1], *camera.get_world_coordinates()[2]))
+    print("\n Coordinates relative to the camera\n", *camera.get_camera_coordinates())
+    print("\n", *camera.get_world_coordinates())
+    print("\n", *ECEF.from_ecef_to_wgs84(camera.get_world_coordinates()[0], camera.get_world_coordinates()[1],
+                                         camera.get_world_coordinates()[2]))
