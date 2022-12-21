b65948824dde7980d6abcae1a2647774d75c6c0a
umlerr <ugrumov.misha@yandex.ru>
2022-12-21 17:22:00 +0300

    Tried to do NED 2 ECEF 2 LLA

--- ECEF.py
+++ ECEF.py
@@ -1,22 +1,50 @@
 import navpy as nv
+import pyproj
+import scipy
+import numpy as np
+from pyproj import proj
 
 
 def from_wgs84_to_ecef(lat, lon, alt):
-    ecef = nv.lla2ecef(lat, lon, alt)
-    return ecef[0], ecef[1], ecef[2]
+    transformer = proj.Transformer.from_crs(
+        {"proj": 'latlong', "ellps": 'WGS84', "datum": 'WGS84'},
+        {"proj": 'geocent', "ellps": 'WGS84', "datum": 'WGS84'},
+    )
+    x, y, z = transformer.transform(lat, lon, alt, radians=False)
+    return x, y, z
 
 
 def from_ecef_to_wgs84(x, y, z):
-    ecef = [x, y, z]
-    lla = nv.ecef2lla(ecef)
-    return lla[0], lla[1], lla[2]
+    transformer = proj.Transformer.from_crs(
+        {"proj": 'geocent', "ellps": 'WGS84', "datum": 'WGS84'},
+        {"proj": 'latlong', "ellps": 'WGS84', "datum": 'WGS84'},
+    )
+    lat, lon, alt = transformer.transform(x, y, z, radians=False)
+    return lat, lon, alt
 
 
-def from_ecef_to_ned(x, y, z, lat, lon, alt):
-    ecef = [x, y, z]
-    ned = nv.ecef2ned(ecef, lat, lon, alt)
-    return ned[0], ned[1], ned[2]
-# 34.00000048333403, -117.33356934722197, 251.70201122574508
-# 34.0000534361111, -117.333547744444, 234.052
+def sed2geodetic(x, y, z, lat_org, lon_org, alt_org):
+    transformer1 = pyproj.Transformer.from_crs(
+        {"proj": 'latlong', "ellps": 'WGS84', "datum": 'WGS84'},
+        {"proj": 'geocent', "ellps": 'WGS84', "datum": 'WGS84'},
+    )
+    transformer2 = pyproj.Transformer.from_crs(
+        {"proj": 'geocent', "ellps": 'WGS84', "datum": 'WGS84'},
+        {"proj": 'latlong', "ellps": 'WGS84', "datum": 'WGS84'},
+    )
 
-# print(nv.lla2ned(34.0000534361111, -117.333547744444, 234.052, 34.00000048333403, -117.33356934722197, 251.70201122574508))
+    x_org, y_org, z_org = transformer1.transform(lon_org, lat_org, alt_org, radians=False)
+    ecef_org = np.array([[x_org, y_org, z_org]]).T
+
+    rot1 = scipy.spatial.transform.Rotation.from_euler('x', -(90-lat_org),
+                                                       degrees=True).as_matrix()  # angle*-1 : left handed *-1
+    rot3 = scipy.spatial.transform.Rotation.from_euler('z', -(90+lon_org),
+                                                       degrees=True).as_matrix()  # angle*-1 : left handed *-1
+
+    rotMatrix = rot1.dot(rot3)
+
+    ecefDelta = rotMatrix.T.dot(np.array([[x, y, z]]).T)
+    ecef = ecefDelta + ecef_org
+    lon, lat, alt = transformer2.transform(ecef[0, 0], ecef[1, 0], ecef[2, 0], radians=False)
+
+    return [lat, lon, alt]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from math import *
 import numpy as np
 import ECEF as ECEF
+import scipy.spatial.transform
 
 
 class Camera:
@@ -14,8 +15,8 @@ class Camera:
         self.focus = f  # focal length(mm) ---> convert to metres
         self.matrix_x = m_a  # matrix side, px
         self.matrix_y = m_b  # matrix side, px
-        self.pixel_x = px  # px - matrix_x/2  # pixel X coordinate in the image, px
-        self.pixel_y = py  # (py - matrix_y/2) * (-1)  # pixel Y coordinate in the image, px
+        self.pixel_x = px  # pixel X coordinate in the image, px
+        self.pixel_y = py  # pixel Y coordinate in the image, px
 
     # Точка в координатах камеры
     def get_camera_coordinates(self):
@@ -32,44 +33,27 @@ class Camera:
             [1]
         ])
         camera_coordinates = intrinsic_matrix_inverse.dot(pixel_coord)
-        #camera_coordinates = camera_coordinates  * self.alt
-        #camera_coordinates[0][0] /= 1000
-        #camera_coordinates[1][0] /= 1000
+        camera_coordinates = camera_coordinates * self.alt
+        camera_coordinates[0][0] /= 1000
+        camera_coordinates[1][0] /= 1000
         return camera_coordinates
 
     # Точка в мировых координатах
     def get_world_coordinates(self):
-        yaw = 0
-        pitch = 0
-        roll = 0
-        drone_x_ecef, drone_y_ecef, drone_z_ecef = ECEF.from_wgs84_to_ecef(self.lat, self.lon, self.alt)
-        # coordinates of camera in world coordinates
-        t = np.array([
-            [drone_x_ecef],
-            [drone_y_ecef],
-            [drone_z_ecef]
-        ])
-        # ecef 2 ned
-        rotate_matrix = np.array([
-            [-sin(self.lat)*cos(self.lon), -sin(self.lat)*sin(self.lon), cos(self.lat)],
-            [-sin(self.lon), cos(self.lon), 0],
-            [-cos(self.lat)*cos(self.lon), -cos(self.lat)*sin(self.lon), -sin(self.lat)]
-        ])
 
         camera_coordinates = camera.get_camera_coordinates()
-        camera_coordinates_ecef = rotate_matrix.dot(camera_coordinates)
 
-        world_coordinates_ecef = camera_coordinates_ecef + t
+        world_coordinates = ECEF.sed2geodetic(camera_coordinates[0], camera_coordinates[1], camera_coordinates[2], self.lat, self.lon, self.alt)
 
-        return world_coordinates_ecef
+        return world_coordinates
 
 
 if __name__ == "__main__":
-    f = 60  # in mm
+    f = 200  # in mm
     matrix_x, matrix_y = 1920, 1080  # in px
-    px = 200
-    py = 1000
-    lat = 59.973017 # degrees
+    px = 1000
+    py = 200
+    lat = 59.973017  # degrees
     lon = 30.220557  # degrees
     alt = 50
     roll = 0
@@ -77,6 +61,4 @@ if __name__ == "__main__":
     yaw = 0
     camera = Camera(lat, lon, alt, roll, pitch, yaw, px, py, f, matrix_x, matrix_y)
     print("\n Coordinates relative to the camera\n", *camera.get_camera_coordinates())
-    print("\n", *camera.get_world_coordinates())
-    print("\n", *ECEF.from_ecef_to_wgs84(camera.get_world_coordinates()[0], camera.get_world_coordinates()[1],
-                                         camera.get_world_coordinates()[2]))
+    print("\n", *camera.get_world_coordinates()) #59.972753, 30.220076
